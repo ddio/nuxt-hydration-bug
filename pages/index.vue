d57d97ee9fe0c89f39eb2920e0555f22e137e2e5
ddio <ddioibb@gmail.com>
2022-05-14 15:53:55 +0800

    feat: add example repo for bug report

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div style="max-width: 50rem;margin: 5rem auto;">
+    <h1> Click links below: </h1>
+    <nuxt-link to="/ㄩㄇ/id0/">id0</nuxt-link><br>
+    <nuxt-link to="/ㄩㄇ/id1/">id1</nuxt-link><br>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
---
+++ pages/ㄩㄇ/_id.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="childPage">
+    <h1> {{payload.title}} </h1>
+    <h2> Payload from asyndData </h2>
+    <div style="margin: 1rem 0;padding: 1rem 0; border: 1px solid #000"> {{payload}}</div>
+    <h2> Check if page javascript is loaded </h2>
+    <div style="border-bottom: 1px solid #000;margin-bottom: 1rem;"> {{textInput}} </div>
+    <input v-model="textInput" />
+  </div>
+</template>
+<script>
+const SOME_DATA = {
+  id0: { title: 'Title of id0' },
+  id1: { title: 'Title of id1' }
+}
+export default {
+  asyncData ({ params }) {
+    const payload = SOME_DATA[params.id] || { title: 'ID not found' }
+    return { payload }
+  },
+  data () {
+    return {
+      textInput: 'some text'
+    }
+  }
+}
+</script>
